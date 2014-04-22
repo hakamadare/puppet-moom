44d54afef403c4977e1cc2c2cbee8463fdcbe6f6
Steve Huff <steve.huff@runkeeper.com>
2014-04-22 16:09:57 -0400

    fixing upstream URL
    also pull a newer version of boxen for unit tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
 # Install Moom.
-class moom {
+class moom (
+  $version = '310',
+) {
   package { 'Moom':
     provider => 'appdmg',
-    source   => 'http://manytricks.com/download/moom',
+    source   => "http://manytricks.com/download/_do_not_hotlink_/moom${version}.dmg",
   }
 }
--- spec/classes/moom_spec.rb
+++ spec/classes/moom_spec.rb
@@ -3,6 +3,6 @@ describe 'moom' do
   it do
     should contain_class('moom')
     should contain_package('Moom').with_provider('appdmg')
-    should contain_package('Moom').with_source('http://manytricks.com/download/moom')
+    should contain_package('Moom').with_source('http://manytricks.com/download/_do_not_hotlink_/moom310.dmg')
   end
 end
