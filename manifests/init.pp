3ab39c826b8f22bc52965c4e75f691231962f939
Steve Huff <shuff@vecna.org>
2014-06-17 00:05:54 -0400

    Set curl user agent via config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,20 @@
 class moom (
   $version = '310',
 ) {
+  $curlrc = "/Users/${::boxen_user}/.curlrc"
+
+  exec { 'set-curl-user-agent':
+    path    => '/bin:/usr/bin',
+    command => "echo 'user-agent = \"Mozilla/4.0\"' >> ${curlrc}",
+    unless  => "grep -q user-agent ${curlrc}",
+  } ->
+  file { $curlrc:
+    ensure  => 'present',
+    content => undef,
+    owner   => $::boxen_user,
+    group   => 'staff',
+    mode    => '0640',
+  } ->
   package { 'Moom':
     provider => 'appdmg',
     source   => "http://manytricks.com/download/_do_not_hotlink_/moom${version}.dmg",
--- spec/classes/moom_spec.rb
+++ spec/classes/moom_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 describe 'moom' do
-  it do
-    should contain_class('moom')
-    should contain_package('Moom').with_provider('appdmg')
-    should contain_package('Moom').with_source('http://manytricks.com/download/_do_not_hotlink_/moom310.dmg')
+  let (:facts) {{ :boxen_user => 'vecna' }}
+
+  describe 'defaults' do
+    it do
+      should contain_class('moom')
+      should contain_exec('set-curl-user-agent')
+      should contain_file('/Users/vecna/.curlrc')
+      should contain_package('Moom').with_provider('appdmg')
+      should contain_package('Moom').with_source('http://manytricks.com/download/_do_not_hotlink_/moom310.dmg')
+    end
+  end
+
+  describe 'version specified' do
+    let (:params) {{ :version => '310' }}
+    it do
+      should contain_class('moom')
+      should contain_exec('set-curl-user-agent')
+      should contain_file('/Users/vecna/.curlrc')
+      should contain_package('Moom').with_provider('appdmg')
+      should contain_package('Moom').with_source('http://manytricks.com/download/_do_not_hotlink_/moom310.dmg')
+    end
   end
 end
